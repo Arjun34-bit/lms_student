8fc90e77c3b7c4b27bb3186c94c8f5be822e42ba
Arjun Madhu Devendra <ARJUNDEVENDRA34@GMAIL.COM>
2025-08-01 15:00:24 +0530

    refactor: simplify Astro configuration by switching to node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,6 @@
-// @ts-check
-import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
+import node from "@astrojs/node";
 
-import vue from "@astrojs/vue";
-
-import svelte from "@astrojs/svelte";
-
-import tailwindcss from "@tailwindcss/vite";
-
-// https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel({
-    imageService: true,
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
-  integrations: [vue(), svelte()],
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  adapter: node({ mode: "standalone" }),
 });
