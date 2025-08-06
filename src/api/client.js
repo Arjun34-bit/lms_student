123886863d34479d03ecbb90068957bc0c412af2
Arjun Madhu Devendra <ARJUNDEVENDRA34@GMAIL.COM>
2025-08-06 20:43:28 +0530

    fix: add console log for API Base URL in client.js

--- src/api/client.js
+++ src/api/client.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const apiBaseUrl = import.meta.env.PROD_PUBLIC_API_BASE_URL;
 
+console.log("API Base URL:", apiBaseUrl);
+
 const api = axios.create({
   baseURL: apiBaseUrl,
   headers: {
